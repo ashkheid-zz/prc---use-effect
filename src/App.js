ce967fa9a660e7c821d4b75f704f0db5981fa9c6
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2022-03-02 16:22:46 +0330

    commit to Main

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,14 +6,22 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const loginCache = localStorage.getItem('loginCache');
 
+    if (loginCache) {
+      setIsLoggedIn(true);
+    }
+  }, []);
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('loginCache', true);
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('loginCache');
     setIsLoggedIn(false);
   };
 
